678469575e24962715f3c952769b1245a5f8d8fe
Marek Kamiński <marek.kaminski.93@gmail.com>
2016-12-04 18:36:32 +0100

    Improved code readability

--- DSManager/ViewModel/Windows/MainViewModel.cs
+++ DSManager/ViewModel/Windows/MainViewModel.cs
@@ -17,53 +17,12 @@ namespace DSManager.ViewModel.Windows {
             NavigateTo(ViewModelLocator.Instance.Students);
         }
 
-        public RelayCommand OpenStudentsPage {
-            get {
-                return _openStudentsPageCommand ?? (_openStudentsPageCommand = new RelayCommand(() => {
-                    NavigateTo(ViewModelLocator.Instance.Students);
-                }));
-            }
-        }
-
-        public RelayCommand OpenInstructorsPage {
-            get {
-                return _openInstructorsPageCommand ?? (_openInstructorsPageCommand = new RelayCommand(() => {
-                    NavigateTo(ViewModelLocator.Instance.Instructors);
-                }));
-            }
-        }
-
-        public RelayCommand OpenCarsPage {
-            get {
-                return _openCarsPageCommand ?? (_openCarsPageCommand = new RelayCommand(() => {
-                    NavigateTo(ViewModelLocator.Instance.Cars);
-                }));
-            }
-        }
-
-        public RelayCommand OpenCoursesPage {
-            get {
-                return _openCoursesPageCommand ?? (_openCoursesPageCommand = new RelayCommand(() => {
-                    NavigateTo(ViewModelLocator.Instance.Courses);
-                }));
-            }
-        }
-
-        public RelayCommand OpenUsersPage {
-            get {
-                return _openUsersPageCommand ?? (_openUsersPageCommand = new RelayCommand(() => {
-                    NavigateTo(ViewModelLocator.Instance.Users);
-                }));
-            }
-        }
-
-        public RelayCommand OpenSettingsPage {
-            get {
-                return _openSettingsPageCommand ?? (_openSettingsPageCommand = new RelayCommand(() => {
-                    NavigateTo(ViewModelLocator.Instance.Settings);
-                }));
-            }
-        }
+        public RelayCommand OpenStudentsPage => _openStudentsPageCommand ?? (_openStudentsPageCommand = new RelayCommand(() => NavigateTo(ViewModelLocator.Instance.Students)));
+        public RelayCommand OpenInstructorsPage => _openInstructorsPageCommand ?? (_openInstructorsPageCommand = new RelayCommand(() => NavigateTo(ViewModelLocator.Instance.Instructors)));
+        public RelayCommand OpenCarsPage => _openCarsPageCommand ?? (_openCarsPageCommand = new RelayCommand(() => NavigateTo(ViewModelLocator.Instance.Cars)));
+        public RelayCommand OpenCoursesPage => _openCoursesPageCommand ?? (_openCoursesPageCommand = new RelayCommand(() => NavigateTo(ViewModelLocator.Instance.Courses)));
+        public RelayCommand OpenUsersPage => _openUsersPageCommand ?? (_openUsersPageCommand = new RelayCommand(() => NavigateTo(ViewModelLocator.Instance.Users)));
+        public RelayCommand OpenSettingsPage => _openSettingsPageCommand ?? (_openSettingsPageCommand = new RelayCommand(() => NavigateTo(ViewModelLocator.Instance.Settings)));
 
         public User User => UserSignedIn.User;
     }
