edb070112788a7224677701a4654a7e4a6ca75ea
Marek Kamiński <marek.kaminski.93@gmail.com>
2016-12-23 15:38:32 +0100

    Added LiveCharts framework Added yearly Income chart

---
+++ DSManager/Converters/ValueToCollapsedNegativeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DSManager.Converters {
+    class ValueToCollapsedNegativeConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if(value is string)
+                return string.IsNullOrEmpty((string)value) ? Visibility.Visible : Visibility.Collapsed;
+            if (value is decimal)
+                return (decimal)value == 0 ? Visibility.Visible : Visibility.Collapsed;
+            if (value is bool)
+                return (bool) value ? Visibility.Collapsed : Visibility.Visible;
+
+            return value == null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DSManager/ViewModel/Pages/StatisticsViewModel.cs
+++ DSManager/ViewModel/Pages/StatisticsViewModel.cs
@@ -1,4 +1,154 @@
-﻿namespace DSManager.ViewModel.Pages {
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+using GalaSoft.MvvmLight.Command;
+
+using LiveCharts;
+using LiveCharts.Wpf;
+
+using NHibernate.Util;
+
+using DSManager.Model.Entities;
+using DSManager.Model.Services;
+
+namespace DSManager.ViewModel.Pages {
     public class StatisticsViewModel : BaseViewModel {
+        #region Variables
+
+        #region Collections
+
+        #region Income
+        private SeriesCollection _incomeCollection;
+        private string[] _incomeLabels;
+        private bool _isIncomeEmpty;
+        #endregion
+
+        #endregion
+
+        #region Commands
+        private RelayCommand _refreshIncome;
+        private RelayCommand _filterIncome;
+        #endregion
+
+        #region View Elements
+        private string _filter;
+        #endregion
+
+        #region Helpers
+        private string _prevFilter;
+        #endregion
+
+        #endregion
+
+        public StatisticsViewModel() {
+            _filter = _prevFilter = string.Empty;
+            FillIncome(_filter);
+        }
+
+        #region Methods
+
+        #region Commands
+        public RelayCommand RefreshIncome {
+            get {
+                return _refreshIncome ?? (_refreshIncome = new RelayCommand(() => {
+                    FillIncome(_prevFilter);
+                }));
+            }
+        }
+
+        public RelayCommand FilterIncome {
+            get {
+                return _filterIncome ?? (_filterIncome = new RelayCommand(() => {
+                    if (_filter.Equals(_prevFilter))
+                        return;
+
+                    FillIncome(_filter);
+                    _prevFilter = _filter;
+                }));
+            }
+        }
+        #endregion
+
+        #region View Elements
+        public string Filter {
+            get { return _filter; }
+            set {
+                if (_filter == value)
+                    return;
+                _filter = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        #region Collections
+
+        #region Income
+
+        public SeriesCollection IncomeCollection {
+            get { return _incomeCollection; }
+            set {
+                _incomeCollection = value;
+                RaisePropertyChanged();
+            }
+        }
+        public string[] IncomeLabels {
+            get { return _incomeLabels; }
+            set {
+                _incomeLabels = value;
+                RaisePropertyChanged();
+            }
+        }
+        public Func<double, string> IncomeFormatter { get; set; }
+
+        public bool IsIncomeEmpty {
+            get { return _isIncomeEmpty; }
+            set {
+                _isIncomeEmpty = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #endregion
+
+        #region Helpers
+        private void FillIncome(string filter) {
+            var firstDayOfYear = string.IsNullOrEmpty(filter) ? new DateTime(DateTime.Now.Year, 1, 1, 0, 0, 0) : new DateTime(int.Parse(filter), 1, 1, 0, 0, 0);
+            var lastDayOfYear = string.IsNullOrEmpty(filter) ? new DateTime(DateTime.Now.Year, 12, 31, 23, 59, 59) : new DateTime(int.Parse(filter), 12, 31, 23, 59, 59);
+
+            var columnSeries = new ColumnSeries {
+                Title = firstDayOfYear.Year.ToString(),
+                Values = new ChartValues<decimal>()
+            };
+            var label = new List<string>();
+
+            using (var repository = new BaseRepository()) {
+                repository.ToList<Payment>()
+                    .Where(x => x.Date >= firstDayOfYear && x.Date <= lastDayOfYear)
+                    .GroupBy(g => g.Date.Month).Select(s => new {
+                        value = s.Sum(amount => amount.Amount),
+                        date = s.First().Date
+                    }).ForEach(x => {
+                            label.Add(x.date.ToString("MMMM", CultureInfo.CurrentCulture));
+                            columnSeries.Values.Add(x.value);
+                        }
+                    );
+            }
+
+            IncomeCollection = new SeriesCollection {columnSeries};
+            IncomeLabels = new string[label.Count];
+            var i = 0;
+            label.ForEach(x => IncomeLabels[i++] = x);
+            IncomeFormatter = value => value + " zł";
+            
+            IsIncomeEmpty = IncomeCollection[0].Values.Count == 0;
+        }
+        #endregion
+
+        #endregion
     }
 }
